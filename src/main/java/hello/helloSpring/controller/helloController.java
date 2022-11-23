f9b53e792b6c21c46a911c349e35ef55f53514cb
lhj910129 <lhj910129@gmail.com>
2022-11-23 16:31:35 +0900

    commit

---
+++ src/main/java/hello/helloSpring/controller/helloController.java
@@ -0,0 +1,25 @@
+package hello.helloSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","spring!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public  String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-tamplate";
+    };
+
+
+
+}
