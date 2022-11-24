40ff48b8b2d057f2e02ce49ac14e9f0fb27913c1
lhj910129 <lhj910129@gmail.com>
2022-11-24 17:11:03 +0900

    commit

---
+++ src/main/java/hello/helloSpring/service/MemberService.java
@@ -0,0 +1,19 @@
+package hello.helloSpring.service;
+
+import hello.helloSpring.repository.MemberRepository;
+import hello.helloSpring.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository() {
+    // 회원가입
+        
+
+
+
+    };
+
+
+
+
+}
