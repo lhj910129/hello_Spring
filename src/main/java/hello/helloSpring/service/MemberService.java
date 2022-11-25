2463d8c1e4e7a0d17a26d4bda64a3b98c5daffd0
lhj910129 <lhj910129@gmail.com>
2022-11-25 17:35:47 +0900

    commit

--- src/main/java/hello/helloSpring/service/MemberService.java
+++ src/main/java/hello/helloSpring/service/MemberService.java
@@ -1,15 +1,45 @@
 package hello.helloSpring.service;
 
+import hello.helloSpring.domain.Member;
 import hello.helloSpring.repository.MemberRepository;
 import hello.helloSpring.repository.MemoryMemberRepository;
 
-public class MemberService {
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService { //ctrl + Shift + T 테스트생성
 
     private final MemberRepository memberRepository = new MemoryMemberRepository() {
-    // 회원가입
-        
 
+        //회원가입
+        private Long join(Member member){
 
+            //ctrl + alt + V            변수로 생성하기
+            //ctrl + shift + alt + T    리팩토링
+
+            validateDuplicateMember(member);
+
+            memberRepository.save(member);
+            return member.getId();
+        }
+
+        //중복회원 거르기
+        //ctrl + alt + M            메소드로 추출하기
+        private void validateDuplicateMember(Member member) {
+            memberRepository.findByName(member.getName()).ifPresent(m -> { //만약 값이 있으면
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+        }
+
+        //전체회원조회
+        private List<Member> findMembers(){
+            return memberRepository.findAll();
+        }
+
+        //Id로 회원 찾기
+        private Optional<Member> findOne(Long memberId){
+            return memberRepository.findById(memberId);
+        }
 
     };
 
---
+++ src/test/java/hello/helloSpring/service/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.helloSpring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입(){ //테스트는 한글로 써도됨
+        //1. given 조건
+
+        //2. when 실행했을때
+
+        //3. then 결과
+    }
+
+    @Test
+    void findMembers(){
+
+    }
+
+    @Test
+    void findOne(){
+
+    }
+}
