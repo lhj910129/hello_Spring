3e19ab76ed3531234bc0654f0517411a3dfab0c8
lhj910129 <lhj910129@gmail.com>
2022-11-28 15:42:14 +0900

    test code 작성

--- src/main/java/hello/helloSpring/service/MemberService.java
+++ src/main/java/hello/helloSpring/service/MemberService.java
@@ -9,41 +9,36 @@ import java.util.Optional;
 
 public class MemberService { //ctrl + Shift + T 테스트생성
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository() {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-        //회원가입
-        private Long join(Member member){
+    //회원가입
+    public Long join(Member member) {
 
-            //ctrl + alt + V            변수로 생성하기
-            //ctrl + shift + alt + T    리팩토링
+        //ctrl + alt + V            변수로 생성하기
+        //ctrl + shift + alt + T    리팩토링
 
-            validateDuplicateMember(member);
+        validateDuplicateMember(member);
 
-            memberRepository.save(member);
-            return member.getId();
-        }
+        memberRepository.save(member);
+        return member.getId();
+    }
 
-        //중복회원 거르기
-        //ctrl + alt + M            메소드로 추출하기
-        private void validateDuplicateMember(Member member) {
-            memberRepository.findByName(member.getName()).ifPresent(m -> { //만약 값이 있으면
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-            });
-        }
+    //중복회원 거르기
+    //ctrl + alt + M            메소드로 추출하기
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> { //만약 값이 있으면
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
 
-        //전체회원조회
-        private List<Member> findMembers(){
-            return memberRepository.findAll();
-        }
+    //전체회원조회
+    public List<Member> findMembers() {
 
-        //Id로 회원 찾기
-        private Optional<Member> findOne(Long memberId){
-            return memberRepository.findById(memberId);
-        }
+        return memberRepository.findAll();
+    }
 
-    };
-
-
-
-
+    //Id로 회원 찾기
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
--- src/test/java/hello/helloSpring/service/MemberServiceTest.java
+++ src/test/java/hello/helloSpring/service/MemberServiceTest.java
@@ -1,5 +1,7 @@
-package hello.helloSpring.service;
+package hello.helloSpring.service; //테스트는 패키지 똑같이 생성됨
 
+import hello.helloSpring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,21 +11,49 @@ class MemberServiceTest {
     MemberService memberService = new MemberService();
 
     @Test
-    void 회원가입(){ //테스트는 한글로 써도됨
-        //1. given 조건
+    void join() { //테스트는 한글로 써도됨!!
+        //1. given (조건)
+        Member member = new Member();
+        member.setName("hello");
 
-        //2. when 실행했을때
+        //2. when (실행했을때)
+        Long saveId = memberService.join(member);
 
-        //3. then 결과
+        //3. then (결과)
+        Member findmember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findmember.getName());
     }
 
     @Test
-    void findMembers(){
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("스프링");
+        Member member2 = new Member();
+        member2.setName("스프링");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+
+        } catch (IllegalStateException e) {
+           // Assertions.assertThat(e.getMessage()).isEqualTo()
+
+
+        }
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
 
     }
 
     @Test
-    void findOne(){
+    void findOne() {
 
     }
 }
